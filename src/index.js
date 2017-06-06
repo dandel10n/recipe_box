c928c3ca5dc2d367709e78e45ab4e643083d58b1
Mariya Radchuk <radchuk.mary@gmail.com>
2017-06-06 20:34:27 +0100

    Add functionality for adding recipes

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ import { storageAvaliable } from './utils';
 const Recipes = (props) => {
     return (
         <div>
-            { props.recipes.map(recipe => {
+            { props.recipes.map((recipe, indexOfRecipe) => {
                 return (
-                    <div key={recipe.id}>
+                    <div key={indexOfRecipe}>
                         <p>{recipe.title}</p>
-                        <p>{recipe.ingredients.map((ingridient, index) => {
-                                return <li key={index}>{ ingridient }</li>
+                        <p>{recipe.ingredients.map((ingredient, indexOfIng) => {
+                                return <li key={indexOfIng}>{ ingredient }</li>
                             })}
                         </p>
                     </div>
@@ -30,17 +30,22 @@ class RecipeBox extends React.Component {
         this.state = {
             recipes: [
                 {
-                    id: 1,
                     title: "Test1",
                     ingredients: ["ing1", "ing2", "ing3"],
-                }, {
-                    id: 2,
+                },
+                {
                     title: "Test2",
                     ingredients: ["ing1", "ing2", "ing3"],
                 }
-            ]
+            ],
+            formTitle: '',
+            formIngredients: []
         }
         this.isStorageAvailible = storageAvaliable('localStorage');
+        this.addRecipe = this.addRecipe.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleTextareaChange = this.handleTextareaChange.bind(this);
     }
 
     componentWillMount() {
@@ -53,18 +58,48 @@ class RecipeBox extends React.Component {
     }
 
     componentDidUpdate() {
-        this.updateLocalStorage();
+        localStorage[this.localStorageKey] = JSON.stringify(this.state.recipes);
     }
 
-    updateLocalStorage() {
-        localStorage.setItem(this.localStorageKey, JSON.stringify(this.state.recipes));
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({[name]: value});
+    }
+
+    handleTextareaChange(event) {
+        const target = event.target;
+        const ingredientsArray = target.value.split(',');
+        /*TODO delete spaces before and after ingredients*/
+        const name = target.name;
+
+        this.setState({[name]: ingredientsArray});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const formTitle = this.state.formTitle;
+        const formIngredients = this.state.formIngredients;
+        this.addRecipe(formTitle, formIngredients);
+    }
+
+    addRecipe(recipeTitle, recipeIngredients)  {
+        const recipes = this.state.recipes;
+        const recipe = {
+            title: recipeTitle,
+            ingredients: recipeIngredients,
+        }
+        const updatedRecipes = recipes.concat([recipe]);
+        this.setState({ recipes: updatedRecipes });
     }
 
     render() {
         if (!this.isStorageAvailible) {
             return (
                 <div>
-                    Your recipes will not be saved becouse
+                    Your recipes will not be saved because
                     of technical problems :(
                 </div>
             )
@@ -72,6 +107,21 @@ class RecipeBox extends React.Component {
             <div className="container">
                 <div>Reciepe Box</div>
                 <Recipes recipes={this.state.recipes}/>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        name="formTitle"
+                        placeholder={"Enter title"}
+                        onChange= {this.handleInputChange}
+                    />
+                    <textarea
+                        type="text"
+                        name="formIngredients"
+                        placeholder={"Enter ingredients"}
+                        onChange= {this.handleTextareaChange}
+                    />
+                    <input type="submit" value="Save" />
+                </form>
             </div>
         );
     }
